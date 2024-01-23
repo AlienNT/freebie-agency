a88b69bbb58a62242a5a4580dc9ecc2da6221c37
Trysten M <nick.tristen22@gmail.com>
2024-01-24 00:22:06 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import {defineConfig} from "vite";
 import postcss from "postcss";
 import autoprefixer from "autoprefixer";
 
-export default defineConfig({
-    plugins:[postcss()],
-    css: {
-        postcss: {
-            plugins: [autoprefixer]
-        }
+export default defineConfig(({command}) => {
+    const base = command === 'build' ? '/freebie-agency' : '/'
+
+    return {
+        plugins: [postcss()],
+        css: {
+            postcss: {
+                plugins: [autoprefixer]
+            }
+        },
+        base
     }
 })
