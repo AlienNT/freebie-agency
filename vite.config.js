5e60058625e9db97a485922ea57d311b68296f6b
Trysten M <nick.tristen22@gmail.com>
2024-01-24 01:01:30 +0200

    fix & update: fix styles and images path

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig(({command}) => {
                 plugins: [autoprefixer]
             }
         },
+        assetsInclude: ['images/*.webp', 'images/*.png'],
         base
     }
 })
